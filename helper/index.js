567c7eeb36c7f5183d2214dc854315812b10dfc6
serialfuzzer <suyogkhanal4@gmail.com>
2023-09-28 13:56:12 +0530

    Added some helper functions to enhance the template a little bit

--- helper/index.js
+++ helper/index.js
@@ -3,7 +3,21 @@ var path = require("path");
 var Helper = function () {}
 
 Helper.prototype = {
-    // additional helper methods
+    combineTargets: function (...args) {
+        var targetsArray = [];
+        args.map( function (arg) {
+            if(Array.isArray(arg)){
+                arg.map(target => {
+                    targetsArray.push(target);
+                })
+            }else{
+                if(arg!= ""){
+                    targetsArray.push(arg);
+                }
+            }
+        })
+        return targetsArray;
+    }
 }
 
 var HelperInstance = new Helper();
