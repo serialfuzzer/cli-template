9b234d36f2b34a977f2062bb598b9394e4853684
serialfuzzer <suyogkhanal4@gmail.com>
2023-09-28 14:01:35 +0530

    Added custom logic to parse targets and make them accessible via a single array

--- index.js
+++ index.js
@@ -21,7 +21,20 @@ cli
 .option("-f, --file <value>", "File to import targets from", "")
 .option("-d, --domain <value>", "Domain to scan", "")
 .action(async function(options){
-        // parse
+    const fileName = options.file; // file name that was passed as a CLI flag
+    const domain = options.domain; // in case target was supplied via the domain flag too
+    var targetsFromFile = []; // array to hold targets from a file, won't be able to load extremely large files, the tool is going to break if you try to import unnecessarily large file
+    if(fileName != ""){
+        targetsFromFile = helper.getTargetsFromFile(fileName); // if file name is not empty, read contents of the file and put it all into the targetsFromFile array
+    }
+    var targets = helper.combineTargets(stdinTargets, targetsFromFile, domain); // all the targets passed via different options (stdin, -f, -d) are combined into a single array so that I could easily work on that array
+    if(targets.length == 0){
+        cli.help() // if number of supplied targets == 0, print cli help
+    } else {
+        // do something with the targets 
+        targets.map(target=>console.log(target))
+    }
+    
 })
 
 
